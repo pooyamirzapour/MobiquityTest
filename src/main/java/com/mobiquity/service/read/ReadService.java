cabec2803e36f554c9f7d975a9f946220cc1d780
Pooyamirzapour <pooyamirzapour@yahoo.com>
2021-06-06 17:59:44 +0430

    init

--- src/main/java/com/mobiquity/service/read/ReadService.java
+++ src/main/java/com/mobiquity/service/read/ReadService.java
@@ -1,12 +1,8 @@
 package com.mobiquity.service.read;
 
 import com.mobiquity.exception.APIException;
-import com.mobiquity.model.Item;
 import com.mobiquity.model.Line;
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.util.List;
 import java.util.stream.Stream;
 
 public interface ReadService {
